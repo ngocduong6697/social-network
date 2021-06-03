da5230546696ce25f85942dba4c5cd93891e7a2b
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-06-03 18:04:12 +0700

    feat: add user registration

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const app = express();
 // connect Database
 connectDB();
 
+// init Middleware
+app.use(express.json({
+    extended: false
+}));
+
+app.get('/', (req, res) => res.send('API running'));
+
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
 
