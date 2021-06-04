e6ce30ec6078884927602f21031d56836227edb3
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-06-04 11:06:33 +0700

    feat: implementing json web token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+// json web token
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -51,6 +54,21 @@ async (req, res) => {
         await user.save();
 
         // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecrect'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        );
         res.send('User registered')
     } catch (err) {
         console.error(err.message);
